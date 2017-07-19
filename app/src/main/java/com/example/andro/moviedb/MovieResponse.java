9dc32d19e735934a383d4736309062c78d34e782
1AboveAll <rawath54@gmail.com>
2017-07-19 21:59:04 +0530

    Added Movies Fragment

---
+++ app/src/main/java/com/example/andro/moviedb/MovieResponse.java
@@ -0,0 +1,22 @@
+package com.example.andro.moviedb;
+
+/**
+ * Created by andro on 19-07-2017.
+ */
+
+public class MovieResponse {
+
+
+
+
+
+
+}
+
+ class results{
+
+     int id;
+
+     float vote;
+
+}
